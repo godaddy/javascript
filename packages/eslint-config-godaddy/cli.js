f5e1ee79329f6cec8c2b43eb708c5521bd82ab7f
indexzero <charlie.robbins@gmail.com>
2017-02-08 01:29:42 -0500

    [fix] Respect any `.eslintrc*` files in the current working directory from which called before forcing config CLI argument passed to `eslint` in `eslint-godaddy**` commands.

--- packages/eslint-config-godaddy/cli.js
+++ packages/eslint-config-godaddy/cli.js
@@ -1,8 +1,12 @@
-const which = require('which');
+const fs = require('fs');
 const path = require('path');
+const which = require('which');
 
 module.exports = function (filename) {
   filename = filename || path.join(__dirname, 'index');
+
+  const isEslint = /^\.eslint.*/;
+  const cwd = process.cwd();
   const has = {
     fix: false,
     config: false
@@ -17,12 +21,24 @@ module.exports = function (filename) {
     process.argv.splice(2, 0, '--fix');
   }
 
-  if (!has.config) {
-    process.argv.splice(2, 0, '-c', require.resolve(filename));
-  }
-
-  which('eslint', function (err, resolved) {
+  //
+  // Only force our config file if there is one not in the current
+  // directory AND not specified by the command line.
+  //
+  fs.readdir(cwd, function (err, files) {
     if (err) { throw err; }
-    require(resolved);
+
+    has.config = has.config || files.some(function (file) {
+      return isEslint.test(file);
+    });
+
+    if (!has.config) {
+      process.argv.splice(2, 0, '-c', require.resolve(filename));
+    }
+
+    which('eslint', function (err, resolved) {
+      if (err) { throw err; }
+      require(resolved);
+    });
   });
 };
