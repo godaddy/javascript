979d5e51eb070a93e2e847dc4436c6024596d3af
Andy Fleming <andy@andyfleming.com>
2020-01-09 16:52:29 -0800

    tweaks dependencies and syntax to pass tests

--- packages/eslint-config-godaddy-react-typescript/index.js
+++ packages/eslint-config-godaddy-react-typescript/index.js
@@ -11,6 +11,7 @@ module.exports = {
       rules: {
         // note you must disable the base rule as it can report incorrect errors (in typescript)
         // (because of imported types being incorrectly reported as unused)
+        // eslint-disable-next-line max-len
         // See https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': [
