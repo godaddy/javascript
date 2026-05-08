5771b986eb43a17d4700c19843d1c132a761c2c0
nlaverde-godaddy <nlaverde@godaddy.com>
2026-05-08 17:26:27 -0500

    test(checkout): add billing validation tests for free pickup

--- packages/react/src/components/checkout/payment/free-payment-form.tsx
+++ packages/react/src/components/checkout/payment/free-payment-form.tsx
@@ -1,6 +1,7 @@
 import { LoaderCircle } from 'lucide-react';
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
+import { AddressForm } from '@/components/checkout/address/address-form';
 import { useCheckoutContext } from '@/components/checkout/checkout';
 import { DeliveryMethods } from '@/components/checkout/delivery/delivery-method';
 import {
@@ -9,14 +10,6 @@ import {
 } from '@/components/checkout/payment/utils/use-confirm-checkout';
 import { useIsPaymentDisabled } from '@/components/checkout/payment/utils/use-is-payment-disabled';
 import { Button } from '@/components/ui/button';
-import {
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
 import { useGoDaddyContext } from '@/godaddy-provider';
 import { GraphQLErrorWithCodes } from '@/lib/graphql-with-errors';
 import { cn } from '@/lib/utils';
@@ -80,42 +73,7 @@ export function FreePaymentForm() {
   if (isPickup) {
     return (
       <div className='space-y-4'>
-        <div className='grid grid-cols-2 gap-4'>
-          <FormField
-            control={form.control}
-            name='billingFirstName'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>{t.shipping.firstName}</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder={t.shipping.firstName}
-                    {...field}
-                    disabled={isConfirmingCheckout}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name='billingLastName'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>{t.shipping.lastName}</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder={t.shipping.lastName}
-                    {...field}
-                    disabled={isConfirmingCheckout}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+        <AddressForm sectionKey='billing' onlyNames />
         {submitButton}
       </div>
     );
