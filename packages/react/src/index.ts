4a9705debe83b594c77e214cdc74a1d7d56d5577
Phil Bennett <pbennett1@godaddy.com>
2026-05-22 10:32:53 -0500

    export currency utilities

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -12,6 +12,13 @@ export {
 } from './components/checkout/line-items/line-items';
 export { useIsPaymentDisabled } from './components/checkout/payment/utils/use-is-payment-disabled';
 export {
+  formatCurrency,
+  convertMajorToMinorUnits,
+  useFormatCurrency,
+  useConvertMajorToMinorUnits,
+  type FormatCurrencyOptions,
+} from './lib/format-currency';
+export {
   DraftOrderTotals,
   type DraftOrderTotalsProps,
   TotalLineItem,
